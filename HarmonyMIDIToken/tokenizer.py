508b5cbe5c6923f5b68c667ae40d322ebfd93c4c
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-08-06 01:16:27 +0900

    Changes to be committed: 토크나이저 아이디어 투두리스트 작성 	modified:   HarmonyMIDIToken/tokenizer.py

--- HarmonyMIDIToken/tokenizer.py
+++ HarmonyMIDIToken/tokenizer.py
@@ -9,6 +9,17 @@ class HarmonyMIDIToken:
         self.melody = []
         self.chords = []
 
+    def _duration_to_note_length(self, dur_quarter_length):
+        mapping = {
+            4.0: "1bar",
+            2.0: "half",
+            1.0: "4th",
+            0.5: "8th",
+            0.25: "16th"
+        }
+        closest = min(mapping.keys(), key=lambda x: abs(x - dur_quarter_length))
+        return mapping[closest]
+
     @property
     def token_id(self):
         """vocab = {
@@ -43,11 +54,15 @@ class HarmonyMIDIToken:
         midi_data = converter.parse(midi_file)
 
         if midi_data.metronomeMarkBoundaries(): # 메트로놈 마크가 있는 경우 첫 번째 마크의 BPM을 사용
-            self.bpm = midi_data.metronomeMarkBoundaries()[0][2].number
+            self.bpm = int(midi_data.metronomeMarkBoundaries()[0][2].number)
 
         for e in midi_data.flat.notesAndRests: # 모든 음표와 쉼표 가져옴
             if isinstance(e, note.Rest):
-                pass
-            
-
-        
+                self.melody.append({'note': '', 'duration': self._duration_to_note_length(e.quarterLength)})
+                self.chords.append({'chord': '', 'duration': self._duration_to_note_length(e.quarterLength)})
+            elif isinstance(e, chord.Chord):
+                # 첫 번째 노트만 남기고 나머지 제거(예시)
+                if len(e.pitches) > 1:
+                    e.pitches = [e.pitches[0]]
+            print(f"Processing element: {e}, offset: {e.offset}, duration: {e.quarterLength}")
+            #TODO: 베이스 싱글 노트를 생각 못함 아마 '공백/베이스노트'로 처리할 것같음, 코드에서 높은 음은 멜로디로 처리하고, 높은 음이 없다면 멜로디에 rest를 추가할 것 멜로디도 같은 방식으로

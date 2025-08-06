1bce00bb64d2bdf8e4b4ff97c34ac4a8706373f5
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-08-07 03:21:12 +0900

    Changes to be committed: bass, melody 토큰화, 디코큰화 성공 	modified:   HarmonyMIDIToken/tokenizer.py 	modified:   test/test.py 	modified:   test/test_output.mid

--- HarmonyMIDIToken/tokenizer.py
+++ HarmonyMIDIToken/tokenizer.py
@@ -11,10 +11,10 @@ class HarmonyMIDIToken:
         self.bass:list[dict] = []
         self._midi = None # MIDI 파일을 저장할 변수 최적화를 위해서임 진짜로 귀찮아서 날먹하는 거 아님
 
-    def _intpitch_to_note_name(self, pitch_int):
+    def _intpitch_to_note_name(self, pitch_int:int) -> str:
         """MIDI 피치 정수를 음표 이름으로 변환합니다."""
         if pitch_int < 0 or pitch_int > 127:
-            raise ValueError("Pitch integer must be between 0 and 127.")
+            return ''  # 유효하지 않은 피치 정수는 빈 문자열로 처리
         pitch_class = pitch_int % 12
         octave = pitch_int // 12 - 1
         note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -66,9 +66,6 @@ class HarmonyMIDIToken:
 
     def _note_list_to_chord(self, note_tuple:tuple[pitch.Pitch]):
         """음표 이름 목록을 코드 표현으로 변환합니다."""
-        if not note_tuple:
-            return ''
-        
         note_list = list(set([n.name.replace("-", "b") for n in note_tuple]))  # 중복 제거 및 b 플랫 처리
         note_list.sort()
         chord = find_chords_from_notes(note_list)
@@ -77,15 +74,79 @@ class HarmonyMIDIToken:
         if "/" in chord_name:
             return chord_name.split("/")[0]  # 코드 이름만 반환
 
-        return chord_name  
+        return chord_name
+    
+    def _note_name_to_intpitch(self, note_name:str) -> int:
+        """음표 이름을 MIDI 피치 정수로 변환합니다."""
+        if note_name == '':
+            return -1
+        pitch_obj = pitch.Pitch(note_name)
+        return pitch_obj.midi
+    
+    def _tokenize(self, data:list[dict]) -> list[int]:
+        """데이터를 토큰화합니다."""
+        tokens = []
+        quality_map = {
+            '':1, 'M': 1, 'm': 2, '7': 3, 'M7': 4, 'm7': 5, 'dim': 6, 'aug': 7,'sus2': 8, 'sus4': 9, "dom7": 10, "half-dim": 11, "dim7": 12, "power": 13,
+        }
+
+        for i in data:
+            key, value = next(iter(i.items()))
+
+            if key == 'note':
+                tokens.append(10)
+                tokens.append(self._note_name_to_intpitch(value))
+                tokens.append(int(i["duration"]*4)) # 4를 곱해서 양자화
+            elif key == 'chord':
+                tokens.append(20)
+                if value == '':
+                    tokens.append(-1)
+                else:
+                    chord = pychord_chord(value)
+
+                    tokens.append(self._note_name_to_intpitch(chord._root+"4"))
+                    try:
+                        tokens.append(quality_map[str(chord._quality)])
+                    except KeyError:
+                        tokens.append(-1)  # 알 수 없는 코드 품질은 -1로 처리
+                    for j in chord._appended:
+                        tokens.append(self._note_name_to_intpitch(j+"4"))
+
+                    tokens.append(int(i["duration"]*4)) # 4를 곱해서 양자화
+        return tokens
+    
+    def _detokenize_note(self, token:list[int]) -> list[dict]:
+        """토큰을 음표로 디토큰화합니다."""
+
+        output = []
+
+        for idx, value in enumerate(token):
+            if value == 10:  # Note 시작
+                output.append({"note":self._intpitch_to_note_name(token[idx+1]), "duration": token[idx+2]/4})
+
+        return output
 
     @property
-    def token_id(self):
+    def token_id(self) -> list[int]:
         """HarmonyMIDIToken에 대한 토큰 ID를 반환한다."""
-        return [self.bpm]
+        melody_tokens = self._tokenize(self.melody)
+        chords_tokens = self._tokenize(self.chords)
+        bass_tokens = self._tokenize(self.bass)
+
+        token = [self.bpm, 100] + melody_tokens +[200]+ chords_tokens +[300]+ bass_tokens
+
+        return token
     
     def set_id(self, token_id) -> None:
         """HarmonyMIDIToken에 대한 토큰 ID를 설정한다."""
+        self.bpm = token_id[0]
+        melody_tokens = token_id[2:token_id.index(200)]
+        chords_tokens = token_id[token_id.index(200)+1:token_id.index(300)]
+        bass_tokens = token_id[token_id.index(300)+1:]
+
+        self.melody = self._detokenize_note(melody_tokens)
+        self.chords = [{"chord": "Ebm", "duration": 0.5}, {"chord": "", "duration": 0.5}, {"chord": "Ebm", "duration": 0.25}]
+        self.bass = self._detokenize_note(bass_tokens)
 
     def to_json(self):
         return json.dumps({
